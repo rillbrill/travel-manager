6c55db5cf1a078ea6493a7cfafe2ba645ee52257
jellysoo97 <jellysoo97@gmail.com>
2024-08-16 21:11:46 +0900

    feat: tailwind config 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,56 @@
+import plugin from 'tailwindcss/plugin'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['var(--font-sans)', 'sans-serif'],
+    },
+    extend: {
+      fontFamily: {
+        logo: 'var(--font-logo)',
+      },
+      // colors
+      colors: {
+        primary: {
+          default: 'var(--teal-400)',
+          50: 'var(--teal-50)',
+          100: 'var(--teal-100)',
+          200: 'var(--teal-200)',
+          300: 'var(--teal-300)',
+          500: 'var(--teal-500)',
+          600: 'var(--teal-600)',
+          700: 'var(--teal-700)',
+          800: 'var(--teal-800)',
+          900: 'var(--teal-900)',
+        },
+        gray: {
+          50: 'var(--gray-50)',
+          100: 'var(--gray-100)',
+          200: 'var(--gray-200)',
+          300: 'var(--gray-300)',
+          400: 'var(--gray-400)',
+          500: 'var(--gray-500)',
+          600: 'var(--gray-600)',
+          700: 'var(--gray-700)',
+          800: 'var(--gray-800)',
+          900: 'var(--gray-900)',
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addUtilities }) =>
+      addUtilities({
+        '.layout': {
+          '@apply mx-auto max-w-3xl min-h-dvh md:max-w-4xl md:flex md:gap-x-8':
+            '',
+        },
+        '.container': {
+          '@apply max-w-sm md:max-w-md': '',
+        },
+      })
+    ),
+  ],
 }
