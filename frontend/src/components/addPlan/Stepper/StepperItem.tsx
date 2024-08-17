f7a5f862e7fe995a124d4539009baf11c472441a
jellysoo97 <jellysoo97@gmail.com>
2024-08-17 21:21:41 +0900

    feat: Stepper 컴포넌트 추가

---
+++ frontend/src/components/addPlan/Stepper/StepperItem.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { FiCheck } from 'react-icons/fi'
+
+import { AddPlanStepEnum } from '@/types'
+import { cn } from '@/utils/cn'
+
+type Props = {
+  step: string
+  icon: React.ReactNode
+  isCurrentStep: boolean
+  isDoneStep: boolean
+}
+
+function StepperItem({ icon, step, isCurrentStep, isDoneStep }: Props) {
+  return (
+    <div className="flex flex-col items-center gap-y-2">
+      <div
+        className={cn([
+          'flex h-10 w-10 items-center justify-center rounded-full',
+          'border-2 text-lg',
+          isCurrentStep || isDoneStep
+            ? 'border-blue-500 text-blue-500'
+            : 'border-gray-400 text-gray-400',
+        ])}
+      >
+        {isDoneStep ? <FiCheck /> : icon}
+      </div>
+      <p
+        className={cn([
+          'text-sm',
+          isCurrentStep || isDoneStep ? 'text-blue-500' : 'text-gray-500',
+        ])}
+      >
+        {step}
+      </p>
+    </div>
+  )
+}
+
+export default StepperItem
