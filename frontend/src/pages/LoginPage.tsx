ae4f243dc9acad6c908a10458dbf4cd251209806
YKDman <dbsrudejr300@gmail.com>
2024-08-17 18:17:42 +0900

    feat : kakao login img 추가

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,5 +1,18 @@
+function KakaoSocoalButton() {
+  return <button>카카오 로그인</button>
+}
+
 function LoginPage() {
-  return <div>Login Page</div>
+  return (
+    <section className="flex h-screen flex-1 flex-col items-center justify-center">
+      <div className="mb-3">
+        <h1 className="font-logo text-3xl">TravelManager</h1>
+      </div>
+      <div>
+        <KakaoSocoalButton />
+      </div>
+    </section>
+  )
 }
 
 export default LoginPage
