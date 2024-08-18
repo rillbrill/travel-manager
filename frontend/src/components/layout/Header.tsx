1eba70c4907407289002dde1a81218cf0b5c050a
jellysoo97 <jellysoo97@gmail.com>
2024-08-18 23:56:23 +0900

    fix: Header 수정

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,6 +1,5 @@
-import { useMemo } from 'react'
 import { FiPlusSquare } from 'react-icons/fi'
-import { Link, useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import { routes } from '@/routes'
 import { AddPlanStepEnum } from '@/types'
@@ -8,20 +7,12 @@ import { AddPlanStepEnum } from '@/types'
 import LogoLink from '../common/LogoLink'
 
 function Header() {
-  const { pathname } = useLocation()
-  const showAddPlanButton = useMemo(
-    () => pathname !== routes.addPlan,
-    [pathname]
-  )
-
   return (
     <header className="mb-4 flex items-center justify-between p-4 shadow-bottom">
       <LogoLink />
-      {showAddPlanButton && (
-        <Link to={`${routes.addPlan}/${AddPlanStepEnum.SetInfo}`}>
-          <FiPlusSquare size="24px" className="text-gray-400" />
-        </Link>
-      )}
+      <Link to={`${routes.addPlan}/${AddPlanStepEnum.SetInfo}`}>
+        <FiPlusSquare size="24px" className="text-gray-400" />
+      </Link>
     </header>
   )
 }
