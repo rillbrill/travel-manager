6498bd1569071d08d9cb1c30af85194edb5a7070
uzyhu <dnwlgn0807@naver.com>
2024-08-19 23:49:16 +0900

    feat: add plan dto

--- backend/src/modules/plan/dto/create-plan.dto.ts
+++ backend/src/modules/plan/dto/create-plan.dto.ts
@@ -1,1 +1,17 @@
-export class CreatePlanDto {}
+import { IsBoolean, IsNumber, IsString } from 'class-validator';
+export class CreatePlanDto {
+    @IsString()
+    plan_name: string;
+
+    @IsString()
+    plan_country: string;
+
+    @IsNumber()
+    head_count: number;
+
+    @IsNumber()
+    total_expenses: number;
+
+    @IsBoolean()
+    plan_end: boolean;
+}
