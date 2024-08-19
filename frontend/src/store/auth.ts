fbbe41f734a40f680853377a8a2cd2a88b1d7587
YKDman <dbsrudejr300@gmail.com>
2024-08-20 01:41:57 +0900

    feat : authStore 구현

---
+++ frontend/src/store/auth.ts
@@ -0,0 +1,45 @@
+import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
+import { immer } from 'zustand/middleware/immer'
+
+type TStoreState = {
+  isLoggedIn: boolean
+}
+
+type TTokenName = 'accessToken' | 'refreshToken'
+type TStoreAction = {
+  authLogin: (tokenValue: string, tokenName: TTokenName) => void
+  authLogout: (tokenName: TTokenName) => void
+}
+
+function getToken() {
+  const accessToken = localStorage.getItem('accessToken')
+  return accessToken
+}
+
+function setToken(tokenValue: string, tokenName: TTokenName) {
+  localStorage.setItem(tokenName, tokenValue)
+}
+
+function removeToken(tokenName: TTokenName) {
+  localStorage.removeItem(tokenName)
+}
+
+export const useAuthStore = create<TStoreState & TStoreAction>()(
+  devtools(
+    immer((set) => ({
+      isLoggedIn: getToken() ? true : false,
+      authLogin: (tokenValue: string, tokenName: TTokenName) =>
+        set((state: TStoreState) => {
+          state.isLoggedIn = true
+          setToken(tokenValue, tokenName)
+        }),
+      authLogout: (tokenName: TTokenName) =>
+        set((state: TStoreState) => {
+          state.isLoggedIn = false
+          removeToken(tokenName)
+        }),
+    })),
+    { name: 'authStore' }
+  )
+)
