eda06470341a677d683e6f3e709ce51a89c96cf8
jellysoo97 <jellysoo97@gmail.com>
2024-08-21 00:57:47 +0900

    styles: 세부 스타일 변경

---
+++ frontend/src/components/addPlan/SetDateForm.tsx
@@ -0,0 +1,45 @@
+import DateRangePicker from '@/components/common/DateRangePicker'
+import { usePlanStore } from '@/store/plan'
+import { DateFormatTypeEnum } from '@/types'
+import { formatDate } from '@/utils/formatDate'
+
+import Button from '../common/Button'
+
+function SetDateForm() {
+  const {
+    plan: { startDate: defaultStartDate, endDate: defaultEndDate },
+    setPlan,
+  } = usePlanStore()
+
+  return (
+    <div className="flex flex-col gap-y-3 sm:gap-y-6">
+      <DateRangePicker
+        defaultStartDate={defaultStartDate}
+        defaultEndDate={defaultEndDate || undefined}
+        onChange={setPlan}
+      />
+      <div className="flex flex-col gap-y-4">
+        <div className="flex items-center gap-x-6">
+          <span className="text-nowrap text-sm text-gray-600">출발일</span>
+          <Button
+            shape="input"
+            className="pointer-events-none p-2 text-center text-gray-800"
+          >
+            {formatDate(DateFormatTypeEnum.DateWithSlash, defaultStartDate)}
+          </Button>
+        </div>
+        <div className="flex items-center gap-x-6">
+          <span className="text-nowrap text-sm text-gray-600">도착일</span>
+          <Button
+            shape="input"
+            className="pointer-events-none p-2 text-center text-gray-800"
+          >
+            {formatDate(DateFormatTypeEnum.DateWithSlash, defaultEndDate)}
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SetDateForm
