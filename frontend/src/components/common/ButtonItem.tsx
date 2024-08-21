377356853faee37ad82c0bb65279c49c4d8b3a13
rillbrill <yd3082929@naver.com>
2024-08-21 19:28:45 +0900

    refactor: 버튼 선택 시 검정 테두리 생성

--- frontend/src/components/common/ButtonItem.tsx
+++ frontend/src/components/common/ButtonItem.tsx
@@ -1,16 +1,28 @@
+import React, { useState } from 'react'
+
 type Props = {
   name: string
   color: string
   onClick: () => void
 }
 const ButtonItem = ({ name, color, onClick }: Props) => {
-  const categoryItemStyleClass = `rounded-lg px-2 py-1 font-sans text-xs`
+  const [isClicked, setIsClicked] = useState(false)
+
+  // 버튼 클릭 시 호출되는 함수
+  const handleClick = () => {
+    setIsClicked(!isClicked)
+    onClick()
+  }
+
+  const categoryItemStyleClass = `mx-1 rounded-lg px-2 py-1 font-sans text-xs ${
+    isClicked ? 'border border-black' : ''
+  }`
 
   return (
     <button
       className={categoryItemStyleClass}
       style={{ backgroundColor: color }}
-      onClick={onClick}
+      onClick={handleClick}
     >
       {name}
     </button>
