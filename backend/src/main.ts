34b9323c20dc7ace84f86604ce5aa9717080d3e4
YKDman <dbsrudejr300@gmail.com>
2024-08-23 13:31:29 +0900

    fix : kakaoAuth PoST 버그 해결

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,9 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+  });
 
   const port = process.env.PORT;
   app.useGlobalPipes(
