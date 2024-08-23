c7ebed91a61e20ef9883ffe8c8f229136fb9b1f9
jellysoo97 <jellysoo97@gmail.com>
2024-08-23 23:42:20 +0900

    feat: 활동 생성 폼 추가

--- frontend/src/constants/errorMessage.ts
+++ frontend/src/constants/errorMessage.ts
@@ -2,4 +2,8 @@ export const errorMessages = {
   addPlan: {
     name: '여행명을 한 글자 이상 입력해주세요.',
   },
+  addActivity: {
+    nameRequired: '활동명을 입력해주세요.',
+    nameMaxLength: '활동명은 15자 이내로 입력해주세요.',
+  },
 }
