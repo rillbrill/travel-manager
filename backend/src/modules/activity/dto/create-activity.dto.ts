44cea37b5b8c2f575d55e17237216d71549a087e
Lmh98 <1004alsgud@gmail.com>
2024-08-23 22:50:43 +0900

    isActivity api별 자동생성으로 삭제

--- backend/src/modules/activity/dto/create-activity.dto.ts
+++ backend/src/modules/activity/dto/create-activity.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsOptional, IsInt, IsBoolean } from 'class-validator';
+import { IsString, IsOptional, IsInt } from 'class-validator';
 
 export class CreateActivityDto {
   @IsString()
@@ -15,9 +15,6 @@ export class CreateActivityDto {
   @IsInt()
   @IsOptional()
   readonly activity_expenses?: number;
-
-  @IsBoolean()
-  readonly isActivity: boolean;
 
   @IsString()
   @IsOptional()
