65e396cc81d6d684d2bf4d3eff9063a0b108332b
DahyeY <dhuniv@naver.com>
2024-08-24 23:54:56 +0900

    refactor: endpoint .env 설정

--- frontend/src/api/Days.ts
+++ frontend/src/api/Days.ts
@@ -2,11 +2,14 @@ import axios from 'axios'
 
 import { Activity, Day, DaysResDto } from '@/types/plan'
 
+const BASE_URL: string =
+  import.meta.env.VITE_BE_BASE_URL || 'http://localhost:3333'
+
 export const fetchDays = async (planId: string): Promise<Day[]> => {
   // const planId = '9e74d62c-3954-41ab-9aca-780c16a61f0d'
   try {
     const response = await axios.get<DaysResDto>(
-      `http://localhost:3333/plans/${planId}/all`
+      `${BASE_URL}/plans/${planId}/all`
     )
 
     const convertData: Day[] = response.data.map((day: Day) => {
@@ -38,7 +41,7 @@ export const deleteActivity = async (
 ): Promise<void> => {
   try {
     await axios.delete(
-      `http://localhost:3333/plans/${planId}/days/${dayId}/activities/${activityId}`
+      `${BASE_URL}/plans/${planId}/days/${dayId}/activities/${activityId}`
     )
     console.log(`Activity with id ${activityId} deleted successfully.`)
   } catch (error) {
