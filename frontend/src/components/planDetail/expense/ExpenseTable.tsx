ecf806dd0a3939a669cc0bcd46d6d9e428a01e33
rillbrill <yd3082929@naver.com>
2024-08-24 21:53:13 +0900

    feat: 비용 테이블 컴포넌트 생성

--- frontend/src/components/planDetail/expense/ExpenseTable.tsx
+++ frontend/src/components/planDetail/expense/ExpenseTable.tsx
@@ -1,5 +1,36 @@
-function ExpenseTable() {
-  return <div>expense table</div>
+import React from 'react'
+
+import { calculateExpenses } from './CaculateExpenses'
+
+type ExpenseTable = {
+  category: string
+  totalCost: number
+  expenseName?: string
+}
+
+const ExpenseTable = ({ expenses }: { expenses: ExpenseTable[] }) => {
+  return (
+    <table className="mr-5">
+      <thead>
+        <tr className="text-right">
+          <th className="pb-2"> </th>
+          <th className="pb-2">₩</th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((expense) => (
+          <tr key={expense.category}>
+            <td className="py-1.5 text-left text-sm font-semibold">
+              {expense.category}
+            </td>
+            <td className="py-1.5 text-right text-sm">
+              {expense.totalCost.toLocaleString('ko-KR')}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
 
 export default ExpenseTable
