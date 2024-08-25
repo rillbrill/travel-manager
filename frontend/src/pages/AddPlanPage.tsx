db7af5e1a2c2e8a7f184d55fba20891da6eb2517
jellysoo97 <jellysoo97@gmail.com>
2024-08-25 14:21:29 +0900

    feat: 여행 추가 api 연동

--- frontend/src/pages/AddPlanPage.tsx
+++ frontend/src/pages/AddPlanPage.tsx
@@ -1,28 +1,33 @@
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
 
+import { plansApi } from '@/api/plans'
 import CheckPlan from '@/components/addPlan/CheckPlan'
 import MoveStepButtons from '@/components/addPlan/MoveStepButtons'
 import SetInfoForm from '@/components/addPlan/SetInfoForm'
 import SetPlaceAndDateForm from '@/components/addPlan/SetPlaceAndDateForm'
 import Stepper from '@/components/addPlan/Stepper'
+import { usePending } from '@/hooks/usePending'
+import { routes } from '@/routes'
 import { usePlanStore } from '@/store/plan'
-import { AddPlanStepEnum } from '@/types'
+import { AddPlanStepEnum, HttpStatusCodeEnum } from '@/types'
 import { AddPlanReqDto } from '@/types/plan'
 
 function AddPlanPage() {
   const [currentStep, setCurrentStep] = useState<number>(
     AddPlanStepEnum.SetInfo
   )
-  const { plan } = usePlanStore()
+  const { plan, resetPlan } = usePlanStore()
   const methods = useForm<Partial<AddPlanReqDto>>({
-    defaultValues: plan,
     mode: 'onChange',
   })
+  const navigate = useNavigate()
+  const { isPending, toggleIsPending } = usePending()
+
   const {
     formState: { errors, isDirty },
   } = methods
-
   const isValid = useMemo(() => {
     switch (currentStep) {
       case AddPlanStepEnum.SetInfo:
@@ -36,26 +41,44 @@ function AddPlanPage() {
   const moveStep = (step: number) => {
     setCurrentStep(step)
   }
+  const handleSubmit = async () => {
+    toggleIsPending(true)
+    const response = await plansApi.addPlan(plan)
+    toggleIsPending(false)
+
+    if (response?.status === HttpStatusCodeEnum.Created) {
+      resetPlan()
+      navigate(routes.plans)
+    } else {
+      alert('여행 생성에 실패했습니다.')
+    }
+  }
+
+  useEffect(() => {
+    resetPlan()
+  }, [])
 
   return (
     <div className="relative flex flex-1 flex-col items-center gap-y-4 p-4">
       <Stepper currentStep={currentStep} />
 
       <FormProvider {...methods}>
-        <form className="flex w-full flex-1 flex-col">
+        <form className="flex w-full flex-1 flex-col justify-between">
           {currentStep === AddPlanStepEnum.SetInfo && <SetInfoForm />}
           {currentStep === AddPlanStepEnum.SetPlaceAndDate && (
             <SetPlaceAndDateForm />
           )}
           {currentStep === AddPlanStepEnum.CheckPlan && <CheckPlan />}
+
+          <MoveStepButtons
+            stepIndex={currentStep}
+            isValid={isValid && isDirty}
+            isLoading={isPending}
+            moveStep={moveStep}
+            handleSubmit={handleSubmit}
+          />
         </form>
       </FormProvider>
-
-      <MoveStepButtons
-        stepIndex={currentStep}
-        isValid={isValid && isDirty}
-        moveStep={moveStep}
-      />
     </div>
   )
 }
