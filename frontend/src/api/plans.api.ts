aa9c1a644a023e750d455236c8b5a33fb551e89d
jellysoo97 <jellysoo97@gmail.com>
2024-08-25 14:20:00 +0900

    fix: api url 수정

--- frontend/src/api/plans.api.ts
+++ frontend/src/api/plans.api.ts
@@ -4,7 +4,7 @@ import { Plan } from '@/models/plan.model'
 
 export const fetchPlans = async (): Promise<Plan[]> => {
   try {
-    const response = await axios.get<Plan[]>('http://localhost:3333/plans')
+    const response = await axios.get<Plan[]>('http://localhost:3333/api/plans')
     return response.data
   } catch (error) {
     console.error('Error fetching plans:', error)
